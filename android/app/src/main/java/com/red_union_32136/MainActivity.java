6b56a7a40b0b1436bde39c8a53123366a1aec912
Crowdbotics <team@crowdbotics.com>
2021-11-25 09:42:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/red_union_32136/MainActivity.java
@@ -0,0 +1,15 @@
+package com.red_union_32136;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "red_union_32136";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "red_union_32136",
+  url: "https://red_union_32136.botics.co",
+  api: "https://red_union_32136.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
